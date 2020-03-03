2fe824984553ffd50f6baa262bcd142fe821b5af
Norton Li <nli@zipcodes-MacBook-Pro.local>
2020-03-03 17:06:42 -0500

    Lab Submission

--- fahrenheit_to_celsius.py
+++ fahrenheit_to_celsius.py
@@ -11,4 +11,4 @@ Fahrenheit =int(input("Enter temperature in Fahrenheit "))
 
 Celsius = (Fahrenheit - 32)*5/9
 
-print(Fahrenheit, "degree fahrenheit is equal ", Celsius, "celsius" )
+print(Fahrenheit, "degree(s) fahrenheit is equal ", Celsius, "degree(s) celsius" )
